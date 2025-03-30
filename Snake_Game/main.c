b5cca99d3bf23bc760053ba36ddcbf4018fd50bc
$ubZ3r0 <106231563+ssbaytri@users.noreply.github.com>
2025-03-30 21:52:50 +0000

    hide the c

--- Snake_Game/main.c
+++ Snake_Game/main.c
@@ -9,6 +9,8 @@ int main()
 	int quit = 0;
 	while (!quit)
 	{
+		// hide the cursor
+		printf("\e[?25l");
 		// draw the maze
 		printf("┌");
 		for (int i = 0; i < COLS; i++)
@@ -26,6 +28,7 @@ int main()
 			printf("─");
 		printf("┘\n");
 
+		// move the cursor to the top
 		printf("\e[%iA", ROWS + 2);
 	}
 	return 0;
