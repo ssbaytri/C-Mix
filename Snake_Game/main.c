e23348bb12dc98f0da7c06ef2af3472eb959a0f1
$ubZ3r0 <106231563+ssbaytri@users.noreply.github.com>
2025-04-02 03:34:04 +0000

    make re

--- Snake_Game/main.c
+++ Snake_Game/main.c
@@ -1,35 +1,8 @@
+#include <curses.h>
 #include <stdio.h>
-#include <stdlib.h>
-
-#define COLS 60
-#define ROWS 30
 
 int main()
 {
-	int quit = 0;
-	while (!quit)
-	{
-		// hide the cursor
-		printf("\e[?25l");
-		// draw the maze
-		printf("┌");
-		for (int i = 0; i < COLS; i++)
-			printf("─");
-		printf("┐\n");
-		for (int j = 0; j < ROWS; j++)
-		{
-			printf("│");
-			for (int i = 0; i < COLS; i++)
-				printf(".");
-			printf("│\n");
-		}
-		printf("└");
-		for (int i = 0; i < COLS; i++)
-			printf("─");
-		printf("┘\n");
-
-		// move the cursor to the top
-		printf("\e[%iA", ROWS + 2);
-	}
+	printf("Hello, World!\n");
 	return 0;
 }
