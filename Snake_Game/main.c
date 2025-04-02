8e00edb71c949873989b868e66c0c60673e2e05d
$ubZ3r0 <106231563+ssbaytri@users.noreply.github.com>
2025-04-02 06:09:47 +0000

    food and score

--- Snake_Game/main.c
+++ Snake_Game/main.c
@@ -1,5 +1,6 @@
 #include <curses.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 typedef struct 
 {
@@ -7,9 +8,11 @@ typedef struct
 	int y;
 } vec2;
 
+int score = 0;
+
 int main()
 {
-	int sh = 40;
+	int sh = 20;
 	int sw = 20;
 
 	WINDOW *win = initscr();
@@ -21,26 +24,32 @@ int main()
 	vec2 head = {0, 0};
 	vec2 dir = {1, 0};
 
+	vec2 food = {rand() % sw, rand() % sh};
+
 	while (true)
 	{
 		int pressed = wgetch(win);
 		if (pressed == KEY_LEFT)
 		{
+			if (dir.x == 1) continue;
 			dir.x = -1;
 			dir.y = 0;
 		}
 		if (pressed == KEY_RIGHT)
 		{
+			if (dir.x == -1) continue;
 			dir.x = 1;
 			dir.y = 0;
 		}
 		if (pressed == KEY_UP)
 		{
+			if (dir.y == 1) continue;
 			dir.x = 0;
 			dir.y = -1;
 		}
 		if (pressed == KEY_DOWN)
 		{
+			if (dir.y == -1) continue;
 			dir.x = 0;
 			dir.y = 1;
 		}
@@ -50,8 +59,16 @@ int main()
 		head.x += dir.x;
 		head.y += dir.y;
 
+		if (head.x == food.x && head.y == food.y)
+		{
+			score++;
+			food.x = rand() % sw;
+			food.y = rand() % sh;
+		}
+
 		erase();
-		mvaddch(head.y, head.x, 'o');
+		mvaddch(food.y, food.x * 2, '@');
+		mvaddch(head.y, head.x * 2, 'O');
 		usleep(125000);
 	}
 	endwin();
