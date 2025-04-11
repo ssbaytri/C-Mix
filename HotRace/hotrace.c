c1e48b3361ee175bc762e18ab4d9ea21d173980d
$ubZ3r0 <106231563+ssbaytri@users.noreply.github.com>
2025-04-11 20:14:38 +0100

    hash, insert search

--- HotRace/hotrace.c
+++ HotRace/hotrace.c
@@ -1,5 +1,16 @@
 #include "hotrace.h"
 
+void	free_table(t_pair *table[])
+{
+	int i;
+
+	for (i = 0; i < TABLE_SIZE; i++)
+	{
+		if (table[i])
+			free(table[i]);
+	}
+}
+
 unsigned long hash_function(char *str)
 {
 	unsigned long hash;
@@ -9,10 +20,10 @@ unsigned long hash_function(char *str)
 	c = 0;
 	while (str[c])
 	{
-		hash = ((hash << 5) + hash) + c;
+		hash = ((hash << 5) + hash) + str[c];
 		c++;
 	}
-	printf("hash: %lu\n", hash);
+	printf("hash value: %lu\n", hash);
 	printf("hash index %lu\n", hash % TABLE_SIZE);
 	return (hash % TABLE_SIZE);
 }
@@ -31,17 +42,45 @@ void	insert(t_pair *table[], char *key, char *value)
 	table[index] = new_pair;
 }
 
+t_pair *search(t_pair *table[], char *key)
+{
+	unsigned long index;
+
+	index = hash_function(key);
+	printf("index: %lu\n", index);
+	printf("hash: %lu\n", hash_function(key));
+	if (table[index] && strcmp(table[index]->key, key) == 0)
+		return (table[index]);
+	return (NULL);
+}
+
+void	ll()
+{
+	system("leaks -q hotrace");
+}
+
 int	main(int ac, char **av)
 {
+	atexit(ll);
+	t_pair *table[TABLE_SIZE];
 	(void)av;
 	if (ac != 1)
 	{
 		write(2, "Usage: ./hotrace\n", 17);
 		return (1);
 	}
-	t_pair *table[TABLE_SIZE] = {NULL};
 	insert(table, "key1", "value1");
 	insert(table, "key2", "value2");
 	insert(table, "key3", "value3");
+	insert(table, "key4", "value4");
+	insert(table, "key5", "value5");
+
+	t_pair *result = search(table, "key1");
+	if (result)
+		printf("Found: %s -> %s\n", result->key, result->value);
+	else
+		printf("Not found\n");
+
+	free_table(table);
 	return (0);
 }
--- HotRace/hotrace.h
+++ HotRace/hotrace.h
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
 
-#define TABLE_SIZE 3
+#define TABLE_SIZE 5
 
 typedef struct s_pair
 {
