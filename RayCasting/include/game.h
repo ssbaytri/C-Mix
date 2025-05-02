c9bbb9e17f75ed1ca159a1cd546805b1fadd8080
$ubZ3r0 <106231563+ssbaytri@users.noreply.github.com>
2025-05-02 23:56:12 +0100

    draw_square

--- RayCasting/include/game.h
+++ RayCasting/include/game.h
@@ -7,11 +7,19 @@
 #include <stdbool.h>
 #include <math.h>
 
+#define WIDTH 1280
+#define HEIGHT 720
+
 typedef struct s_game
 {
 	void *mlx;
 	void *win;
 	void *img;
+
+	char *data;
+	int bpp;
+	int size_line;
+	int endian;
 }	t_game;
 
 #endif
--- RayCasting/src/main.c
+++ RayCasting/src/main.c
@@ -1,10 +1,35 @@
 #include "../include/game.h"
 
+void put_pixel(int x, int y, int color, t_game *game)
+{
+    if(x >= WIDTH || y >= HEIGHT || x < 0 || y < 0)
+        return;
+    
+    int index = y * game->size_line + x * game->bpp / 8;
+    game->data[index] = color & 0xFF;
+    game->data[index + 1] = (color >> 8) & 0xFF;
+    game->data[index + 2] = (color >> 16) & 0xFF;
+}
+
+void draw_square(int x, int y, int size, int color, t_game *game)
+{
+    for(int i = 0; i < size; i++)
+        put_pixel(x + i, y, color, game);
+    for(int i = 0; i < size; i++)
+        put_pixel(x, y + i, color, game);
+    for(int i = 0; i < size; i++)
+        put_pixel(x + size, y + i, color, game);
+    for(int i = 0; i < size; i++)
+        put_pixel(x + i, y + size, color, game);
+}
+
 void	init_game(t_game *game)
 {
 	game->mlx = mlx_init();
-	game->win = mlx_new_window(game->mlx, 1280, 720, "raycasting");
-	game->img = mlx_new_image(game->mlx, 1280, 720);
+	game->win = mlx_new_window(game->mlx, WIDTH, HEIGHT, "raycasting");
+	game->img = mlx_new_image(game->mlx, WIDTH, HEIGHT);
+
+	game->data = mlx_get_data_addr(game->img, &game->bpp, &game->size_line, &game->endian);
 }
 
 int main(void)
@@ -12,6 +37,8 @@ int main(void)
 	t_game game;
 
 	init_game(&game);
+	draw_square(WIDTH / 2, HEIGHT / 2, 20, 0x00FF00, &game);
+	mlx_put_image_to_window(game.mlx, game.win, game.img, 0, 0);
 	mlx_loop(game.mlx);
 	return (0);
 }
