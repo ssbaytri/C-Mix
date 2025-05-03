5f5ef8e0cb7736a63bbfb7b0a602a612e63de087
ssbaytri <azjoker333@gmail.com>
2025-05-03 06:36:10 +0100

    player

--- RayCasting/include/game.h
+++ RayCasting/include/game.h
@@ -10,6 +10,24 @@
 #define WIDTH 1280
 #define HEIGHT 720
 
+#define W 119
+#define A 97
+#define S 115
+#define D 100
+
+#define PI 3.14159265358979323846
+
+typedef struct s_player
+{
+	float x;
+	float y;
+
+	bool key_up;
+	bool key_down;
+	bool key_left;
+	bool key_right;
+} t_player;
+
 typedef struct s_game
 {
 	void *mlx;
@@ -20,6 +38,14 @@ typedef struct s_game
 	int bpp;
 	int size_line;
 	int endian;
-}	t_game;
+	t_player player;
+} t_game;
+
+
+void	init_player(t_player *player);
+int key_press(int keycode, t_player *player);
+int key_release(int keycode, t_player *player);
+void    move_player(t_player *player);
+
 
 #endif
--- RayCasting/src/main.c
+++ RayCasting/src/main.c
@@ -25,6 +25,7 @@ void draw_square(int x, int y, int size, int color, t_game *game)
 
 void	init_game(t_game *game)
 {
+    init_player(&game->player);
 	game->mlx = mlx_init();
 	game->win = mlx_new_window(game->mlx, WIDTH, HEIGHT, "raycasting");
 	game->img = mlx_new_image(game->mlx, WIDTH, HEIGHT);
@@ -34,6 +35,24 @@ void	init_game(t_game *game)
         &game->size_line,
         &game->endian
     );
+    mlx_put_image_to_window(game->mlx, game->win, game->img, 0, 0);
+}
+
+void    clear_img(t_game *game)
+{
+    for (int y = 0; y < HEIGHT; y++)
+        for (int x = 0; x < WIDTH; x++)
+            put_pixel(x, y, 0x000000, game);
+}
+
+int draw_loop(t_game *game)
+{
+    t_player *player = &game->player;
+    move_player(player);
+    clear_img(game);
+    draw_square(player->x, player->y, 20, 0xFF0000, game);
+    mlx_put_image_to_window(game->mlx, game->win, game->img, 0, 0);
+    return (0);
 }
 
 int main(void)
@@ -41,8 +60,9 @@ int main(void)
 	t_game game;
 
 	init_game(&game);
-	draw_square(WIDTH / 2, HEIGHT / 2, 20, 0xFF0000, &game);
-	mlx_put_image_to_window(game.mlx, game.win, game.img, 0, 0);
+    mlx_hook(game.win, 2, 1L<<0, key_press, &game.player);
+    mlx_hook(game.win, 3, 1L<<1, key_release, &game.player);
+    mlx_loop_hook(game.mlx, draw_loop, &game);
 	mlx_loop(game.mlx);
 	return (0);
 }
