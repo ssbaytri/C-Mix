fedab22a4c176298a92c17d9dfb9b9a12e9c5983
$ubZ3r0 <106231563+ssbaytri@users.noreply.github.com>
2025-09-07 20:56:32 +0100

    TBE

--- Mini_text_editor/main.c
+++ Mini_text_editor/main.c
@@ -1,7 +1,31 @@
 #include <stdio.h>
+#include <string.h>
 
-int main(void)
+void edit_line(char *buffer, int curr_line)
 {
-	printf("Getting started\n");
+	for (int i = 0; i < curr_line; i++)
+		buffer = strchr(buffer, '\n') + 1;
+	char *line_end = strchr(buffer, '\n');
+	char saved[1024] = {0};
+	strcpy(saved, buffer);
+	scanf("%s", buffer);
+	strcpy(buffer + strlen(buffer), saved);
+}
+
+int main(int argc, char **argv)
+{
+	FILE* file = fopen(argv[1], "r");
+	char buffer[1024] = {0};
+	fread(buffer, 1024, 1, file);
+	fclose(file);
+
+	int current_line = 0;
+	printf("Contents: \n%s\n", buffer);
+	scanf("%d", &current_line);
+	edit_line(buffer, current_line);
+	file = fopen(argv[1], "w");
+	fwrite(buffer, strlen(buffer), 1, file);
+	fclose(file);
+
 	return (0);
 }
