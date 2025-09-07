259d0968133aa59576476b3e4d04b4a6c634b396
ssbaytri <azjoker333@gmail.com>
2025-09-07 22:50:46 +0100

    simple as that

--- Mini_text_editor/main.c
+++ Mini_text_editor/main.c
@@ -1,19 +1,27 @@
 #include <stdio.h>
 #include <string.h>
 
-void edit_line(char *buffer, int curr_line)
+int edit_line(char *buffer, int curr_line)
 {
 	for (int i = 0; i < curr_line; i++)
-		buffer = strchr(buffer, '\n') + 1;
+	{
+		buffer = strchr(buffer, '\n');
+		if (!buffer)
+			return (printf("Invalid Line\n"), 0);
+		buffer = buffer + 1;
+	}
 	char *line_end = strchr(buffer, '\n');
 	char saved[1024] = {0};
-	strcpy(saved, buffer);
+	strcpy(saved, line_end);
 	scanf("%s", buffer);
-	strcpy(buffer + strlen(buffer), saved);
+	strcat(buffer, saved);
+	return (1);
 }
 
 int main(int argc, char **argv)
 {
+	if (argc != 2)
+		return (printf("./editor: [filename]\n"), 1);
 	FILE* file = fopen(argv[1], "r");
 	char buffer[1024] = {0};
 	fread(buffer, 1024, 1, file);
@@ -22,7 +30,8 @@ int main(int argc, char **argv)
 	int current_line = 0;
 	printf("Contents: \n%s\n", buffer);
 	scanf("%d", &current_line);
-	edit_line(buffer, current_line);
+	if (!edit_line(buffer, current_line))
+		return (1);
 	file = fopen(argv[1], "w");
 	fwrite(buffer, strlen(buffer), 1, file);
 	fclose(file);
